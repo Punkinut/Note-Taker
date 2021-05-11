955f8c4cd2e5d581001356bc24fd95f165ea83da
Punkinut <heypunkinut@gmail.com>
2021-05-12 09:33:34 +1000

    Added dash parameter that returns the user to the home page

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
