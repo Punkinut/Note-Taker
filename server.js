66c10d02cc5a89cba01f1b5666557450e886c3c8
Punkinut <heypunkinut@gmail.com>
2021-05-13 09:56:01 +1000

    The post data is pushed into an array and written to the database file

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+let noteArr = [];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,8 +19,9 @@ app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'
 
 app.post('/api/notes', (req, res) => {
     const saveNote = req.body;
+    noteArr.push(saveNote);
     res.json(saveNote);
-    fs.writeFile('test.json', JSON.stringify(saveNote, null, 2), (err) => 
+    fs.writeFile('db/db.json', JSON.stringify(noteArr, null, 2), (err) => 
     err ? console.log(err) : console.log('Success'))
 });
 
