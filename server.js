e7ffbcd7c9c6da0a103bff068153df45143a9a5a
Punkinut <heypunkinut@gmail.com>
2021-05-13 13:45:57 +1000

    Updated json file

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ app.get('/api/notes', (req, res) => {
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
